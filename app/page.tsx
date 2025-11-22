93b8086fd452e5a1bcffdddf3739f07915bfd4e9
Emil <emilchobanyab@gmail.com>
2025-11-22 13:39:03 +0400

    Category card UI finished

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import Button from "@/src/components/Button";
+
 export default function Home() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-zinc-50 font-sans dark:bg-black">
@@ -8,6 +10,9 @@ export default function Home() {
         <p className="mt-4 text-lg text-gray-600 dark:text-gray-300">
           Your one-stop shop for all things campus life. Explore our wide range of products and enjoy a seamless shopping experience.
         </p>
+        <Button variant="primary" size="sm">Create Your First Listing</Button>
+          <Button variant="secondary" size="md">Create Your First Listing</Button>
+            <Button variant="ghost" size="lg">Create Your First Listing</Button>
       </main>
     </div>
   );
